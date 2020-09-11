a320913de4ece8ccbdd88233c3772082cc7ee9ca
Kethan Vegunta <kethan@vegunta.com>
2020-09-10 21:05:28 -0700

    removed unncessary folders

--- Example/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    quotes: 'double',
-  },
-};
